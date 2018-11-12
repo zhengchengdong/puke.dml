1ec4481f642af4aca28a5985bc142af6f6b7c2dd
林少聪 < @PC-20180515PRDG>
2018-11-12 17:52:22 +0800

    继续开发

---
+++ src/main/java/com/dml/puke/pai/QiShouLiangPaiMark.java
@@ -0,0 +1,24 @@
+package com.dml.puke.pai;
+
+/**
+ * 起手亮牌标记
+ * 
+ * @author lsc
+ *
+ */
+public class QiShouLiangPaiMark implements PukePaiMark {
+	private String name = "qishouLiangPai";
+
+	public String name() {
+		return name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ src/main/java/com/dml/puke/pai/ZuDuiLiangPaiMark.java
@@ -0,0 +1,23 @@
+package com.dml.puke.pai;
+
+/**
+ * 组队亮牌标记
+ * 
+ * @author lsc
+ *
+ */
+public class ZuDuiLiangPaiMark implements PukePaiMark {
+	private String name = "zuduiLiangPai";
+
+	public String name() {
+		return name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
