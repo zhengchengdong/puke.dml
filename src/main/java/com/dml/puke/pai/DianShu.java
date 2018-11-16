e44a08d1dfda383733296e1fc45b30bd7c1e0307
林少聪 < @PC-20180515PRDG>
2018-11-16 18:22:46 +0800

    继续开发

--- src/main/java/com/dml/puke/pai/DianShu.java
+++ src/main/java/com/dml/puke/pai/DianShu.java
@@ -2,4 +2,9 @@ package com.dml.puke.pai;
 
 public enum DianShu {
 	san, si, wu, liu, qi, ba, jiu, shi, J, Q, K, A, er, xiaowang, dawang;
+
+	public static DianShu getDianShuByOrdinal(int ordinal) {
+		DianShu[] dianshuZu = DianShu.values();
+		return dianshuZu[ordinal];
+	}
 }
--- src/main/java/com/dml/puke/wanfa/dianshu/dianshuzu/DianShuZuGenerator.java
+++ src/main/java/com/dml/puke/wanfa/dianshu/dianshuzu/DianShuZuGenerator.java
@@ -1,6 +1,9 @@
 package com.dml.puke.wanfa.dianshu.dianshuzu;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import com.dml.puke.pai.DianShu;
 
 /**
  * 通过点数数量数组生成相关的所有可能点数组
@@ -9,39 +12,118 @@ import java.util.List;
  *
  */
 public class DianShuZuGenerator {
-	public static List<DanzhangDianShuZu> generatAllDanzhangDianShuZu(int[] dianShuAmountArray) {
-		// TODO
-		return null;
+
+	public static List<DanzhangDianShuZu> generateAllDanzhangDianShuZu(int[] dianShuAmountArray) {
+		List<DanzhangDianShuZu> danzhangList = new ArrayList<>();
+		for (int i = 0; i < dianShuAmountArray.length; i++) {
+			int dianshuCount = dianShuAmountArray[i];
+			if (dianshuCount >= 1) {
+				DanzhangDianShuZu danzhangDianShuZu = new DanzhangDianShuZu(DianShu.getDianShuByOrdinal(i));
+				danzhangList.add(danzhangDianShuZu);
+			}
+		}
+		return danzhangList;
 	}
 
-	public static List<DuiziDianShuZu> generatAllDuiziDianShuZu(int[] dianShuAmountArray) {
-		// TODO
-		return null;
+	public static List<DuiziDianShuZu> generateAllDuiziDianShuZu(int[] dianShuAmountArray) {
+		List<DuiziDianShuZu> duiziList = new ArrayList<>();
+		for (int i = 0; i < dianShuAmountArray.length; i++) {
+			int dianshuCount = dianShuAmountArray[i];
+			if (dianshuCount >= 2) {
+				DuiziDianShuZu duiziDianShuZu = new DuiziDianShuZu(DianShu.getDianShuByOrdinal(i));
+				duiziList.add(duiziDianShuZu);
+			}
+		}
+		return duiziList;
 	}
 
-	public static List<LianduiDianShuZu> generatAllLianduiDianShuZu(int[] dianShuAmountArray, int length) {
-		// TODO
-		return null;
+	public static List<LianduiDianShuZu> generateAllLianduiDianShuZu(int[] dianShuAmountArray, int length) {
+		List<LianduiDianShuZu> lianduiList = new ArrayList<>();
+		for (int i = 0; i < dianShuAmountArray.length; i++) {
+			int danzhangLianXuCount = 0;
+			int j = i;
+			while (j < 12 && dianShuAmountArray[j] >= 2) {// 3对或者以上的点数相连的牌，如556677；2和大、小王无法参与
+				danzhangLianXuCount++;
+				j++;
+			}
+			if (danzhangLianXuCount >= length) {
+				DianShu[] lianXuDianShuArray = new DianShu[danzhangLianXuCount];
+				for (int k = 0; k < danzhangLianXuCount; k++) {
+					lianXuDianShuArray[k] = DianShu.getDianShuByOrdinal(i + k);
+				}
+				LianduiDianShuZu liandui = new LianduiDianShuZu(lianXuDianShuArray);
+				lianduiList.add(liandui);
+			}
+		}
+		return lianduiList;
 	}
 
-	public static List<LiansanzhangDianShuZu> generatAllLiansanzhangDianShuZu(int[] dianShuAmountArray, int length) {
-		// TODO
-		return null;
+	public static List<LiansanzhangDianShuZu> generateAllLiansanzhangDianShuZu(int[] dianShuAmountArray, int length) {
+		List<LiansanzhangDianShuZu> lianSanZhangList = new ArrayList<>();
+		for (int i = 0; i < dianShuAmountArray.length; i++) {
+			int danzhangLianXuCount = 0;
+			int j = i;
+			while (j < 12 && dianShuAmountArray[j] >= 3) {// 3个或者3个以上的点数相连的3张牌，如555666777；2和大、小王无法参与
+				danzhangLianXuCount++;
+				j++;
+			}
+			if (danzhangLianXuCount >= length) {
+				DianShu[] lianXuDianShuArray = new DianShu[danzhangLianXuCount];
+				for (int k = 0; k < danzhangLianXuCount; k++) {
+					lianXuDianShuArray[k] = DianShu.getDianShuByOrdinal(i + k);
+				}
+				LiansanzhangDianShuZu lianSanZhang = new LiansanzhangDianShuZu(lianXuDianShuArray);
+				lianSanZhangList.add(lianSanZhang);
+			}
+		}
+		return lianSanZhangList;
 	}
 
-	public static List<SanzhangDianShuZu> generatAllSanzhangDianShuZu(int[] dianShuAmountArray) {
-		// TODO
-		return null;
+	public static List<SanzhangDianShuZu> generateAllSanzhangDianShuZu(int[] dianShuAmountArray) {
+		List<SanzhangDianShuZu> sanzhangList = new ArrayList<>();
+		for (int i = 0; i < dianShuAmountArray.length; i++) {
+			int dianshuCount = dianShuAmountArray[i];
+			if (dianshuCount >= 3) {
+				SanzhangDianShuZu sanzhangDianShuZu = new SanzhangDianShuZu(DianShu.getDianShuByOrdinal(i));
+				sanzhangList.add(sanzhangDianShuZu);
+			}
+		}
+		return sanzhangList;
 	}
 
-	public static List<ShunziDianShuZu> generatAllShunziDianShuZu(int[] dianShuAmountArray, int length) {
-		// TODO
-		return null;
+	public static List<ShunziDianShuZu> generateAllShunziDianShuZu(int[] dianShuAmountArray, int length) {
+		List<ShunziDianShuZu> shunziList = new ArrayList<>();
+		for (int i = 0; i < dianShuAmountArray.length; i++) {
+			int danzhangLianXuCount = 0;
+			int j = i;
+			while (j < 12 && dianShuAmountArray[j] >= 1) {// 任意5张或者5张以上点数相连的牌，3起最小，到A最大；无2和大、小王
+				danzhangLianXuCount++;
+				j++;
+			}
+			if (danzhangLianXuCount >= length) {
+				DianShu[] lianXuDianShuArray = new DianShu[danzhangLianXuCount];
+				for (int k = 0; k < danzhangLianXuCount; k++) {
+					lianXuDianShuArray[k] = DianShu.getDianShuByOrdinal(i + k);
+				}
+				ShunziDianShuZu shunzi = new ShunziDianShuZu(lianXuDianShuArray);
+				shunziList.add(shunzi);
+			}
+		}
+		return shunziList;
 	}
 
-	public static List<ZhadanDianShuZu> generatAllZhadanDianShuZu(int[] dianShuAmountArray) {
-		// TODO
-		return null;
+	public static List<ZhadanDianShuZu> generateAllZhadanDianShuZu(int[] dianShuAmountArray) {
+		List<ZhadanDianShuZu> zhadanList = new ArrayList<>();
+		for (int i = 0; i < dianShuAmountArray.length; i++) {
+			int dianshuCount = dianShuAmountArray[i];
+			if (dianshuCount >= 4) {
+				for (int j = 4; j < dianshuCount; j++) {
+					ZhadanDianShuZu zhadanDianShuZu = new ZhadanDianShuZu(DianShu.getDianShuByOrdinal(i), j);
+					zhadanList.add(zhadanDianShuZu);
+				}
+			}
+		}
+		return zhadanList;
 	}
 
 }
